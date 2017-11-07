26ca1d91040f8574923928773a68492521395b8e
Aaron Colaco <aaron_colaco@persistent.co.in>
2017-11-07 16:42:02 +0530

    Add function to return homogenous error messages

--- users/controller.js
+++ users/controller.js
@@ -1,8 +1,12 @@
 'use strict';
 
 const getAllUsers = (req, res) => {
-  return res.json({"message": "Return all users"});
+  return res.json({ "message": "Return all users" });
 };
+
+
+const errorResponse = (res, message, error, status = 500) =>
+  res.status(status).json({ "status": status, "message": message, "error": error });
 
 
 module.exports = {
